899bc7036db5c5e45b9d82259c76b301877ad991
OksanaZaga <oksanazag7@gmail.com>
2026-02-25 15:14:39 -0500

    Update merge1.js

--- merge1.js
+++ merge1.js
@@ -2,8 +2,7 @@ function merge(arr1, arr2){
   const results = [];
   let i=0;
   let j=0;
-let arr1 = [1,3,4,5];
-let arr2 = [2,4,6,8];
+
 while(i<arr1.length && j<arr2.length){
 
       if(arr1[i]< arr2[j]){
@@ -12,11 +11,22 @@ while(i<arr1.length && j<arr2.length){
       } else {
       results.push(arr2[j]);
       j++;
-    }} return results;
-} merge(arr1,arr2) // [1,2,3,4,4,5,6,8]
+    }
+    while(i< arr1.length){
+        results.push(arr1[i]);
+        i++;
+    }
+    while(j<arr2.length){
+        results.push(arr2[j]);
+        j++;
+    }
+} return results;
+} 
+let arr1 = [1,3,4,5];
+let arr2 = [2,4,6,8];
+merge(arr1,arr2) // [1,2,3,4,4,5,6,8]
+console.log(merge(arr1,arr2));
 
-//arr1 // [1,3,4,5];
-//arr2 // [2,4,6,8];
 
 let arr3 = [-2,-1,0,4,5,6];
 let arr4 = [-3,-2,-1,2,3,5,7,8];
