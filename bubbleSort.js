e3d8dcdf2a136f8e3a7f4e704be9cba21958497b
OksanaZaga <oksanazag7@gmail.com>
2026-02-24 21:00:30 -0500

    myfirstBubble

---
+++ bubbleSort.js
@@ -0,0 +1,32 @@
+function bubbleSort() {}
+
+//module.exports = bubbleSort;
+bubbleSort([4, 20, 12, 10, 7, 9]); // [4, 7, 9, 10, 12, 20]
+bubbleSort([0, -10, 7, 4]); // [-10, 0, 4, 7]
+bubbleSort([1, 2, 3]); // [1, 2, 3]
+bubbleSort([]);
+
+ function bubbleSort(arr) 
+ {
+    for (let i=0; i < arr.length; i++) { 
+
+        for (let j=0; j<arr.length -i-1; j++){
+            if (arr[j]>arr[j+1]){
+             let temp=arr[j];
+             arr[j] = arr[j+1];
+             arr[j+1]= temp;
+            }
+        }console.log(arr);
+        }
+    }
+let nums = [
+    4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34,
+    23, 2, 453, 546, 75, 67, 4342, 32];                
+
+console.log("Original array:", nums);
+
+bubbleSort(nums); // Call the function with the array
+console.log("Sorted array:", nums); // Log the sorted array
+//bubbleSort(nums); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34, 35, 43, 67,
+                  // 75, 232, 232, 453, 546, 4342]
+ 
